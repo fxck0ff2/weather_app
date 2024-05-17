8ecd778464702eabf71ae85614f61f4d30d3e83b
fxck0ff2 <hyperpc60@gmail.com>
2024-05-18 00:03:25 +0300

    Delete src directory

--- src/components/IncorrectCity.jsx
+++
@@ -1,8 +0,0 @@
-export default function IncorrectCity({ state }) {
-  
-  return(
-    <span id="errorModal" state={`${state}`}>
-      Такого миста не існує, або не правильна назва міста
-    </span>
-  )
-};
